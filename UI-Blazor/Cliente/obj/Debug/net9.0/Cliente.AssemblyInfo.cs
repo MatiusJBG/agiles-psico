3f6ccb8c56cf6ee57d734e8a9c018814a67edf36
LuisMiranda0 <luis.miranda.20.12.2004@gmail.com>
2025-10-20 23:25:22 -0500

    Trabajo Final

--- UI-Blazor/Cliente/obj/Debug/net9.0/Cliente.AssemblyInfo.cs
+++ UI-Blazor/Cliente/obj/Debug/net9.0/Cliente.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cliente")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32254108b44a8158833cec4a89f49ab0e565e41f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b4871e96b848ec39f4ef8cf2cc9d3155f94b5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cliente")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cliente")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
